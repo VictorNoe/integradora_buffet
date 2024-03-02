0d3f6e0492f8db7ae8db25c0c5e39563d04ed0f4
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-03-01 21:18:11 -0600

    Change new script to database

--- buffet/src/main/resources/db/migration/V1__first_database.sql
+++ buffet/src/main/resources/db/migration/V1__first_database.sql
@@ -1,89 +1,100 @@
 
 CREATE TABLE IF NOT EXISTS user_type (
-id_user_type INT PRIMARY KEY NOT NULL auto_increment,
-type_name VARCHAR(50) NOT NULL
+    id_user_type binary(16) PRIMARY KEY NOT NULL,
+    type_name VARCHAR(50) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS status(
-id_status INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-status_name VARCHAR(100) NOT NULL);
+    id_status binary(16) NOT NULL PRIMARY KEY,
+    status_name VARCHAR(100) NOT NULL);
 
 
 CREATE TABLE IF NOT EXISTS user_info (
-id_user_info INT PRIMARY KEY NOT NULL auto_increment,
- name VARCHAR(20) NOT NULL,
-lastname VARCHAR(20) NOT NULL,
-surname VARCHAR(20) NOT NULL,
-phone VARCHAR(15) NOT NULL,
-created_at DATETIME NOT NULL,
-modified_at DATETIME,
-fk_user_type int NOT NULL,
-FOREIGN KEY (fk_user_type) REFERENCES user_type(id_user_type)
+    id_user_info binary(16) PRIMARY KEY NOT NULL ,
+    name VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    surname VARCHAR(20) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    created_at DATETIME NOT NULL,
+    modified_at DATETIME,
+    fk_user_type binary(16) NOT NULL,
+    FOREIGN KEY (fk_user_type) REFERENCES user_type(id_user_type)
     );
 
 CREATE TABLE IF NOT EXISTS user_account (
-id_user_account INT PRIMARY KEY NOT NULL auto_increment,
-email VARCHAR(30) UNIQUE NOT NULL,
-password VARCHAR(100) NOT NULL,
-token VARCHAR(100) NOT NULL,
-login_time DATETIME,
-logout_time DATETIME,
-fk_user_info int,
-fk_status int,
-FOREIGN KEY (fk_user_info) REFERENCES user_info(id_user_info),
-FOREIGN KEY (fk_status) REFERENCES status(id_status)
-);
+    id_user_account binary(16) PRIMARY KEY NOT NULL ,
+    email VARCHAR(30) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    token VARCHAR(100) NOT NULL,
+    login_time DATETIME,
+    logout_time DATETIME,
+    fk_user_info binary(16),
+    fk_status binary(16),
+    FOREIGN KEY (fk_user_info) REFERENCES user_info(id_user_info),
+    FOREIGN KEY (fk_status) REFERENCES status(id_status)
+    );
 
 CREATE TABLE IF NOT EXISTS service_category (
-id_category INT PRIMARY KEY NOT NULL auto_increment,
-category_name VARCHAR(50) NOT NULL
-);
+    id_category binary(16) PRIMARY KEY NOT NULL,
+    category_name VARCHAR(50) NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS service_package (
-id_package INT PRIMARY KEY NOT NULL auto_increment,
-package_name VARCHAR(100) NOT NULL,
-package_description VARCHAR(300),
-price DOUBLE NOT NULL,
-discount DOUBLE,
-created_at DATETIME NOT NULL,
-modified_at DATETIME,
-fk_category int NOT NULL,
-fk_status int NOT NULL,
-FOREIGN KEY (fk_category) REFERENCES service_category(id_category),
-FOREIGN KEY (fk_status) REFERENCES status(id_status)
-);
+    id_package binary(16) PRIMARY KEY NOT NULL,
+    package_name VARCHAR(100) NOT NULL,
+    package_description VARCHAR(300),
+    price DOUBLE NOT NULL,
+    discount DOUBLE,
+    ability INT NOT NULL,
+    created_at DATETIME NOT NULL,
+    modified_at DATETIME,
+    fk_category binary(16) NOT NULL,
+    fk_status binary(16) NOT NULL,
+    FOREIGN KEY (fk_category) REFERENCES service_category(id_category),
+    FOREIGN KEY (fk_status) REFERENCES status(id_status)
+    );
 
 CREATE TABLE IF NOT EXISTS payment_method(
-id_payment_method INT PRIMARY KEY NOT NULL auto_increment,
-name VARCHAR(100) NOT NULL,
-description VARCHAR(100) NOT NULL,
-fk_status INT NOT NULL,
-FOREIGN KEY (fk_status) REFERENCES status(id_status)
-);
+    id_payment_method binary(16) PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    fk_status binary(16) NOT NULL,
+    FOREIGN KEY (fk_status) REFERENCES status(id_status)
+    );
 
 CREATE TABLE IF NOT EXISTS package_order (
-id_order INT PRIMARY KEY NOT NULL auto_increment,
-num_order INT NOT NULL,
-order_date DATETIME,
-order_price DOUBLE NOT NULL,
-street VARCHAR(30) NOT NULL,
-distric VARCHAR(30) NOT NULL,
-postal_code INT(8) NOT NULL,
-city VARCHAR(30) NOT NULL,
-comments VARCHAR(100),
-fk_user_account int NOT NULL,
-fk_package int NOT NULL,
-fk_status int NOT NULL,
-fk_payment_method int,
-FOREIGN KEY (fk_user_account) REFERENCES user_account(id_user_account),
-FOREIGN KEY (fk_package) REFERENCES service_package(id_package),
-FOREIGN KEY (fk_status) REFERENCES status(id_status),
-FOREIGN KEY (fk_payment_method) REFERENCES payment_method(id_payment_method)
-);
+    id_order binary(16) PRIMARY KEY NOT NULL,
+    num_order varchar(20) NOT NULL,
+    order_date DATETIME,
+    order_price DOUBLE NOT NULL,
+    street VARCHAR(30) NOT NULL,
+    distric VARCHAR(30) NOT NULL,
+    postal_code INT(8) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    comments VARCHAR(100),
+    fk_user_account binary(16) NOT NULL,
+    fk_package binary(16) NOT NULL,
+    fk_status binary(16) NOT NULL,
+    fk_payment_method binary(16),
+    FOREIGN KEY (fk_user_account) REFERENCES user_account(id_user_account),
+    FOREIGN KEY (fk_package) REFERENCES service_package(id_package),
+    FOREIGN KEY (fk_status) REFERENCES status(id_status),
+    FOREIGN KEY (fk_payment_method) REFERENCES payment_method(id_payment_method)
+    );
 CREATE TABLE IF NOT EXISTS worker_assignment (
-id_assigment INT PRIMARY KEY NOT NULL auto_increment,
-fk_user_account INT NOT NULL,
-fk_order int NOT NULL,
-FOREIGN KEY (fk_user_account) REFERENCES user_account(id_user_account),
-FOREIGN KEY (fk_order) REFERENCES package_order(id_order)
-);
+    id_assigment binary(16) PRIMARY KEY NOT NULL,
+    fk_user_account binary(16) NOT NULL,
+    fk_order binary(16) NOT NULL,
+    FOREIGN KEY (fk_user_account) REFERENCES user_account(id_user_account),
+    FOREIGN KEY (fk_order) REFERENCES package_order(id_order)
+    );
+CREATE TABLE IF NOT EXISTs package_has_comment(
+    id_package_has_comment binary(16) NOT NULL,
+    comment_text VARCHAR(50) NOT NULL,
+    comment_point int,
+    comment_date DATETIME NOT NULL,
+    fk_user_account binary(16) not null,
+    fk_package binary(16) NOT NULL,
+    FOREIGN KEY (fk_user_account) REFERENCES user_account(id_user_account),
+    FOREIGN KEY (fk_package) REFERENCES service_package(id_package)
+    );
--- buffet/src/main/resources/db/migration/V2__data_insersion.sql
+++ buffet/src/main/resources/db/migration/V2__data_insersion.sql
@@ -1,18 +1,20 @@
-INSERT INTO user_type (id_user_type, type_name) VALUES    (1, 'Administrator'),
-                                                          (2, 'Worker'),
-                                                          (3, 'Public');
-INSERT INTO status
-VALUES     (1, 'enable'),
-           (2, 'disabled'),
-           (3, 'disabled'),
-           (4, 'accepted'),
-           (5, 'sold'),
-           (6, 'required');
-INSERT INTO user_info (name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES ('Christian','Carrasco', 'Alonso','7778189101', '2024-02-11 19:35:36', '2024-02-11 19:35:36', '1');
-INSERT INTO user_info (name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES ('Victor','Flores', 'Arriaga','777213101', '2024-02-11 19:35:36', '2024-02-11 19:35:36', '2');
-INSERT INTO user_info (name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES ('Karel','Perez', 'Mendoza','77712121', '2024-02-11 19:35:36', '2024-02-11 19:35:36', '3');
+INSERT INTO user_type (id_user_type, type_name) VALUES (X'1530FD96D83F11EE944BD812655E2906', 'Administrator');
+INSERT INTO user_type (id_user_type, type_name) VALUES (X'21E60A80D83F11EE944BD812655E2906', 'Worker');
+INSERT INTO user_type (id_user_type, type_name) VALUES (X'2765C5E1D83F11EE944BD812655E2906', 'Public');
 
-INSERT INTO user_account (email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES ('c@gmail.com', 'Buffet1!', 'admin','2024-02-11 19:35:36','2024-02-11 19:35:36', '1', '1');
-INSERT INTO user_account (email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES ('v@gmail.com', 'Buffet1!', 'worker','2024-02-11 19:35:36','2024-02-11 19:35:36', '2', '1');
-INSERT INTO user_account (email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES ('k@gmail.com', 'Buffet1!', 'public','2024-02-11 19:35:36','2024-02-11 19:35:36', '3', '1');
+INSERT INTO status
+VALUES     (X'2765C5E1D83F11EE944BD812655E2909', 'enable'),
+           (X'2765C5E1D83F11EE944BD812655E2911', 'disabled'),
+           (X'2765C5E1D83F11EE944BD812655E2921', 'disabled'),
+           (X'2765C5E1D83F11EE944BD812655E2901', 'accepted'),
+           (X'2765C5E1D83F11EE944BD812655E2913', 'sold'),
+           (X'2765C5E1D83F11EE944BD812655E2943', 'required');
+
+INSERT INTO user_info (id_user_info,name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES (X'2765C5E1D83F11EE944BD812655E2911','Christian','Carrasco', 'Alonso','7778189101', '2024-02-11 19:35:36', '2024-02-11 19:35:36', X'2765C5E1D83F11EE944BD812655E2906');
+INSERT INTO user_info (id_user_info,name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES (X'2765C5E1D83F11EE944BD812655E2922','Victor','Flores', 'Arriaga','777213101', '2024-02-11 19:35:36', '2024-02-11 19:35:36', X'21E60A80D83F11EE944BD812655E2906');
+INSERT INTO user_info (id_user_info,name, lastname,surname,phone, created_at, modified_at, fk_user_type) VALUES (X'2765C5E1D83F11EE944BD812655E2933','Karel','Perez', 'Mendoza','77712121', '2024-02-11 19:35:36', '2024-02-11 19:35:36', X'2765C5E1D83F11EE944BD812655E2906');
+
+INSERT INTO user_account (id_user_account,email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES (X'2765C5E1D83F11EE944BD812655E2921','c@gmail.com', 'Buffet1!', 'admin','2024-02-11 19:35:36','2024-02-11 19:35:36', X'2765C5E1D83F11EE944BD812655E2911', X'2765C5E1D83F11EE944BD812655E2909');
+INSERT INTO user_account (id_user_account,email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES (X'2765C5E1D83F11EE944BD812655E2922','v@gmail.com', 'Buffet1!', 'worker','2024-02-11 19:35:36','2024-02-11 19:35:36', X'2765C5E1D83F11EE944BD812655E2922', X'2765C5E1D83F11EE944BD812655E2909');
+INSERT INTO user_account (id_user_account,email, password, token,login_time,logout_time, fk_user_info, fk_status   ) VALUES (X'2765C5E1D83F11EE944BD812655E2923','k@gmail.com', 'Buffet1!', 'public','2024-02-11 19:35:36','2024-02-11 19:35:36', X'2765C5E1D83F11EE944BD812655E2933', X'2765C5E1D83F11EE944BD812655E2909');
 
