4c47c7520e726756e75b5177cabd267d8d21ed3e
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-03-04 15:38:45 -0600

    Fix package DTO

---
+++ buffet/src/main/java/com/buffet/buffet/controller/Package/PackageDTO/PackageDTO.java
@@ -0,0 +1,31 @@
+package com.buffet.buffet.controller.Package.PackageDTO;
+
+import com.buffet.buffet.model.category.Category;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+public class PackageDTO {
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "El nombre no debe contener caracteres especiales")
+    @NotEmpty(message = "El nombre no puede estar en blanco")
+    private String packageName;
+    @NotEmpty(message = "La imagen no puede estar vacia")
+    private String image;
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "La descripcion no debe contener caracteres especiales")
+    @NotEmpty(message = "La descripcion no puede estar en blanco")
+    private String packageDescription;
+    @NotNull(message = "El precio no puede estar vacio")
+    private double price;
+    private double discount;
+    private Integer ability;
+    @NotNull(message = "Debes agregar una categoria")
+    private String category;
+
+}
